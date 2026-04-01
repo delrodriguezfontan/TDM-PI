151cb43821e5b3ddedb90856b8072d33f38afed1
Florencia Abril Bang <flor@Florencias-MacBook-Pro.local>
2026-04-01 17:49:01 -0300

    Estructura basica de screens peliculas y series

--- src/screens/Peliculas/Peliculas.js
+++ src/screens/Peliculas/Peliculas.js
@@ -1,8 +1,85 @@
-import React from "react";
+function Peliculas() {
+  return (
+    <React.Fragment> 
+      <h2 className="alert alert-primary">Todas las películas</h2>
 
+      <button className="btn btn-info">Cargar más</button>
 
+      <section className="row cards all-movies" id="movies">
+        <article className="single-card-movie">
+          <img
+            src="https://image.tmdb.org/t/p/w500/tzrJulItjttxzoX0t3B2My46TS7.jpg"
+            className="card-img-top"
+            alt="movie"
+          />
+          <div className="cardBody">
+            <h5 className="card-title">The Thursday Murder Club</h5>
+            <p className="card-text">
+              A group of senior sleuths passionate about solving cold cases...
+            </p>
+            <a href="movie.html" className="btn btn-primary">Ver más</a>
+          </div>
+        </article>
 
+        <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/9PXZIUsSDh4alB80jheWX4fhZmy.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">F1</h5>
+                    <p className="card-text">Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling
+                        Formula 1 team—and mentor a young hotshot driver—while chasing one more chance at glory.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                </div>
+            </article>
 
-
+            <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/A06yXys3hrCWu8xiNoHCFLTG5SH.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">I Know What You Did Last Summer</h5>
+                    <p className="card-text">When five friends inadvertently cause a deadly car accident, they cover up
+                        their involvement and make a pact to keep it a secret rather than face the consequences. A year
+                        later, their past comes back to haunt them and they're forced to confront a horrifying truth:
+                        someone knows what they did last summer…and is hell-bent on revenge.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                </div>
+            </article>
+            <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg" class="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">Superman</h5>
+                    <p className="card-text">Superman, a journalist in Metropolis, embarks on a journey to reconcile his
+                        Kryptonian heritage with his human upbringing as Clark Kent.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                </div>
+            </article>
+                        <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/9PXZIUsSDh4alB80jheWX4fhZmy.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">F1</h5>
+                    <p className="card-text">Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling
+                        Formula 1 team—and mentor a young hotshot driver—while chasing one more chance at glory.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                </div>
+            </article>
+            <article className="single-card-movie">
+                <img src="https://image.tmdb.org/t/p/w500/A06yXys3hrCWu8xiNoHCFLTG5SH.jpg" className="card-img-top"
+                    alt="..."/>
+                <div className="cardBody">
+                    <h5 className="card-title">I Know What You Did Last Summer</h5>
+                    <p className="card-text">When five friends inadvertently cause a deadly car accident, they cover up
+                        their involvement and make a pact to keep it a secret rather than face the consequences. A year
+                        later, their past comes back to haunt them and they're forced to confront a horrifying truth:
+                        someone knows what they did last summer…and is hell-bent on revenge.</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                </div>
+            </article>
+        
+      </section>
+    </React.Fragment> 
+  );
+}
 
 export default Peliculas;
