1f755326cdbcd23006a1cec899511186057eef5f
delrodriguezfontan <drodriguezfontan@udesa.edu.ar>
2026-04-01 17:46:22 -0300

    corrigiendo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 function Footer (){
     return(
         <footer className="alert alert-primary mt-4 text-center">
-        <p className="mb-0">Integrante 1 | Integrante 2 | Integrante 3</p>
+        <p className="mb-0">Florencia Bang | Delfina Rodriguez Fontan | Nadine Ruiz</p>
     </footer>
     );
 }
