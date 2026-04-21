03320f2d3491c17842fe7fbd3292c51a898702a8
NadineRUIZ <nruizbaudouin@udesa.edu.ar>
2026-04-21 00:19:00 -0300

    css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Footer.css";
 function Footer (){
     return(
-        <footer className="alet alert-primary mt-4 text-center">
+        <footer className="alert-primary mt-4 text-center">
         <p className="mb-0">Nadine Ruiz | Delfina Rodriguez Fontan  </p>
     </footer>
     );
